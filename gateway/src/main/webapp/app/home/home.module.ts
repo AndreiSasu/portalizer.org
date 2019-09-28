5a3bf1893a13b41df294e352dab02ebe3641f219
Andrei Sasu <andrei.sasu42@gmail.com>
2019-09-28 20:35:19 +0100

    Add material stuff

--- gateway/src/main/webapp/app/home/home.module.ts
+++ gateway/src/main/webapp/app/home/home.module.ts
@@ -5,10 +5,8 @@ import { GatewaySharedModule } from 'app/shared/shared.module';
 import { HOME_ROUTE } from './home.route';
 import { HomeComponent } from './home.component';
 
-import { MatSliderModule } from '@angular/material/slider';
-
 @NgModule({
-  imports: [GatewaySharedModule, MatSliderModule, RouterModule.forChild([HOME_ROUTE])],
+  imports: [GatewaySharedModule, RouterModule.forChild([HOME_ROUTE])],
   declarations: [HomeComponent]
 })
 export class GatewayHomeModule {}
