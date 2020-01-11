d58e676706d8344c0c4e7e703cc0f2c867e17071
Andrei Sasu <andrei.sasu42@gmail.com>
2020-01-11 11:23:18 +0000

    Works

--- monolithic/src/main/webapp/app/layouts/footer/footer.component.ts
+++ monolithic/src/main/webapp/app/layouts/footer/footer.component.ts
@@ -1,7 +1,15 @@
 import { Component } from '@angular/core';
+import { faLinkedin, faGithub, faAngular, faAws, faJava, faDocker } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'jhi-footer',
   templateUrl: './footer.component.html'
 })
-export class FooterComponent {}
+export class FooterComponent {
+  faGithub = faGithub;
+  faLinkedin = faLinkedin;
+  faAngular = faAngular;
+  faAws = faAws;
+  faJava = faJava;
+  faDocker = faDocker;
+}
