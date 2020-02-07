d09a33847e2e4358d0b03897e8abd06e59229735
Andrei Sasu <andrei.sasu42@gmail.com>
2020-02-07 18:06:47 +0000

    misc stuff

--- monolithic/src/main/webapp/app/retro/searchbar/searchbar.component.ts
+++ monolithic/src/main/webapp/app/retro/searchbar/searchbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'jhi-searchbar',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./searchbar.component.scss']
 })
 export class SearchbarComponent implements OnInit {
+  faSearch = faSearch;
+
   constructor() {}
 
   ngOnInit() {}
