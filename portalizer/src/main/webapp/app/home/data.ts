a9390c7118747cc72005155a2412efbb1b748adc
Andrei Sasu <andrei.sasu42@gmail.com>
2020-05-31 13:12:59 +0100

    front page mockup

---
+++ portalizer/src/main/webapp/app/home/data.ts
@@ -0,0 +1,14 @@
+export const single = [
+  {
+    name: 'Total boards',
+    value: 500
+  },
+  {
+    name: 'Total cards',
+    value: 3500
+  },
+  {
+    name: 'Total actionable items',
+    value: 1123
+  }
+];
